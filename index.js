fa88e3bfd8e60d7fb7e7294ee44ba60a3ecb3282
Pedro Rigo <pedrorigooliveira@hotmail.com>
2024-06-19 17:11:40 -0300

    Atualizações no código

--- index.js
+++ index.js
@@ -1,57 +1,39 @@
 const symbolInput = document.querySelector('#symbol');
 const suggestionsBox = document.querySelector('#suggestions');
-const stockList = document.querySelector('#stock-list');
 const form = document.getElementById('myForm');
-const stockChart = document.getElementById('stockChart').getContext('2d');
-const stockName = document.getElementById('stock-name');
-const stockPrice = document.getElementById('stock-price');
-const stockChange = document.getElementById('stock-change');
+const stockChartCtx = document.getElementById('stockChart').getContext('2d');
+const cryptoChartCtx = document.getElementById('cryptoChart').getContext('2d');
+const indicesChartCtx = document.getElementById('indicesChart').getContext('2d');
+const commoditiesChartCtx = document.getElementById('commoditiesChart').getContext('2d');
+const currenciesChartCtx = document.getElementById('currenciesChart').getContext('2d');
 const tabs = document.querySelectorAll('.tab-button');
 const tabContents = document.querySelectorAll('.tab-content');
-let chart;
 
-const apiKey = 'd4706445-df94-47ba-aa97-6cfb184e4d13';
+let stockChart, cryptoChart, indicesChart, commoditiesChart, currenciesChart;
 
-function fetchTopStocks() {
-    fetch('https://www.alphavantage.co/query?function=SECTOR&apikey=EBEDU7X5Z0G444A0')
-        .then(response => response.json())
-        .then(data => {
-            const stocks = data['Rank A: Real-Time Performance'];
-            if (!stocks) {
-                stockList.innerHTML = '<tr class="error"><td colspan="6">Erro ao obter dados de ações</td></tr>';
-                return;
-            }
-            let html = '';
-            let labels = [];
-            let dataPoints = [];
-            for (let i = 0; i < 10; i++) {
-                const symbol = Object.keys(stocks)[i];
-                const change = stocks[symbol];
-                const changeColor = parseFloat(change) >= 0 ? 'green' : 'red';
-                labels.push(symbol);
-                dataPoints.push(parseFloat(change));
-                html += `
-                    <tr>
-                        <td>${symbol}</td>
-                        <td style="color: ${changeColor}">${change}</td>
-                    </tr>
-                `;
-            }
-            stockList.innerHTML = html;
-            renderChart(labels, dataPoints);
-        })
-        .catch(error => {
-            console.error(error);
-            stockList.innerHTML = '<tr class="error"><td colspan="6">Erro ao obter dados de ações</td></tr>';
-        });
+document.addEventListener('DOMContentLoaded', function () {
+    loadDefaultData();
+    tabs.forEach(tab => tab.addEventListener('click', handleTabSwitch));
+});
+
+function loadDefaultData() {
+    fetchStockData('AAPL');
+    fetchCryptoData('bitcoin');
 }
 
-function fetchStockData(symbol) {
-    if (!symbol || symbol.trim() === '') {
-        fetchTopStocks();
-        return;
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const symbol = symbolInput.value.trim();
+    const activeTab = document.querySelector('.tab-button.active').dataset.tab;
+    if (activeTab === 'stocks') {
+        fetchStockData(symbol);
+    } else if (activeTab === 'crypto') {
+        fetchCryptoData(symbol);
     }
+    // FETCH DO RESTO AQUI!!!!!!!!!
+});
 
+function fetchStockData(symbol) {
     fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=EBEDU7X5Z0G444A0`)
         .then(response => response.json())
         .then(data => {
@@ -60,50 +42,33 @@ function fetchStockData(symbol) {
                 const price = quote['05. price'];
                 const changePercent = quote['10. change percent'].replace('%', '');
                 const changeColor = parseFloat(changePercent) >= 0 ? 'green' : 'red';
-                stockName.textContent = `Ação: ${symbol}`;
-                stockPrice.textContent = `Preço Atual: $${price}`;
-                stockChange.textContent = `Variação: ${changePercent}%`;
-                stockChange.style.color = changeColor;
+                document.getElementById('stock-name').textContent = `Ação: ${symbol}`;
+                document.getElementById('stock-price').textContent = `Preço Atual: $${price}`;
+                document.getElementById('stock-change').textContent = `Variação: ${changePercent}%`;
+                document.getElementById('stock-change').style.color = changeColor;
 
-                const html = `
-                    <tr>
-                        <td>${symbol}</td>
-                        <td style="color: ${changeColor}">${changePercent}</td>
-                    </tr>
-                `;
-                stockList.innerHTML = html;
-                renderChart([symbol], [parseFloat(changePercent)]);
-                fetchHistoricalData(symbol);
+                fetchHistoricalData(symbol, stockChartCtx, 'stockChart', data => renderChart('stockChart', stockChartCtx, data));
             } else {
-                stockName.textContent = '';
-                stockPrice.textContent = '';
-                stockChange.textContent = '';
-                stockList.innerHTML = '<tr class="error"><td colspan="6">Símbolo Inválido</td></tr>';
+                document.getElementById('stock-name').textContent = '';
+                document.getElementById('stock-price').textContent = '';
+                document.getElementById('stock-change').textContent = '';
+                document.getElementById('stock-list').innerHTML = '<tr class="error"><td colspan="6">Símbolo Inválido</td></tr>';
             }
         })
         .catch(error => {
             console.error(error);
-            stockName.textContent = '';
-            stockPrice.textContent = '';
-            stockChange.textContent = '';
-            stockList.innerHTML = '<tr class="error"><td colspan="6">Erro ao obter dados da ação</td></tr>';
+            document.getElementById('stock-list').innerHTML = '<tr class="error"><td colspan="6">Erro ao obter dados da ação</td></tr>';
         });
 }
 
-function fetchHistoricalData(symbol) {
+function fetchHistoricalData(symbol, chartContext, chartType, callback) {
     fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=EBEDU7X5Z0G444A0`)
         .then(response => response.json())
         .then(data => {
             const timeSeries = data['Time Series (Daily)'];
-            if (!timeSeries) {
-                stockList.innerHTML = '<tr class="error"><td colspan="6">Erro ao obter dados históricos</td></tr>';
-                return;
-            }
-
-            let html = '';
             const labels = [];
             const dataPoints = [];
-
+            let html = '';
             Object.keys(timeSeries).slice(0, 30).forEach(date => {
                 const dayData = timeSeries[date];
                 html += `
@@ -119,27 +84,81 @@ function fetchHistoricalData(symbol) {
                 labels.unshift(date);
                 dataPoints.unshift(parseFloat(dayData['4. close']));
             });
-
-            stockList.innerHTML = html;
-            renderChart(labels, dataPoints);
+            document.getElementById(`${chartType}-list`).innerHTML = html;
+            callback({ labels, dataPoints });
         })
         .catch(error => {
             console.error(error);
-            stockList.innerHTML = '<tr class="error"><td colspan="6">Erro ao obter dados históricos</td></tr>';
+            document.getElementById(`${chartType}-list`).innerHTML = '<tr class="error"><td colspan="6">Erro ao obter dados históricos</td></tr>';
         });
 }
 
-function renderChart(labels, data) {
-    if (chart) {
-        chart.destroy();
+function fetchCryptoData(symbol) {
+    fetch(`https://api.coingecko.com/api/v3/coins/${symbol}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data) {
+                const price = data.market_data.current_price.usd;
+                const changePercent = data.market_data.price_change_percentage_24h;
+                const changeColor = changePercent >= 0 ? 'green' : 'red';
+                document.getElementById('crypto-name').textContent = `Criptomoeda: ${data.name}`;
+                document.getElementById('crypto-price').textContent = `Preço Atual: $${price}`;
+                document.getElementById('crypto-change').textContent = `Variação: ${changePercent}%`;
+                document.getElementById('crypto-change').style.color = changeColor;
+
+                fetchCryptoHistoricalData(symbol, cryptoChartCtx, 'cryptoChart', data => renderChart('cryptoChart', cryptoChartCtx, data));
+            } else {
+                document.getElementById('crypto-name').textContent = '';
+                document.getElementById('crypto-price').textContent = '';
+                document.getElementById('crypto-change').textContent = '';
+                document.getElementById('crypto-list').innerHTML = '<tr class="error"><td colspan="6">Símbolo Inválido</td></tr>';
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            document.getElementById('crypto-list').innerHTML = '<tr class="error"><td colspan="6">Erro ao obter dados da criptomoeda</td></tr>';
+        });
+}
+
+function fetchCryptoHistoricalData(symbol, chartContext, chartType, callback) {
+    fetch(`https://api.coingecko.com/api/v3/coins/${symbol}/market_chart?vs_currency=usd&days=30`)
+        .then(response => response.json())
+        .then(data => {
+            const prices = data.prices;
+            const labels = [];
+            const dataPoints = [];
+            let html = '';
+            prices.forEach(price => {
+                const date = new Date(price[0]).toISOString().split('T')[0];
+                html += `
+                    <tr>
+                        <td>${date}</td>
+                        <td>${price[1]}</td>
+                    </tr>
+                `;
+                labels.push(date);
+                dataPoints.push(price[1]);
+            });
+            document.getElementById(`${chartType}-list`).innerHTML = html;
+            callback({ labels, dataPoints });
+        })
+        .catch(error => {
+            console.error(error);
+            document.getElementById(`${chartType}-list`).innerHTML = '<tr class="error"><td colspan="6">Erro ao obter dados históricos</td></tr>';
+        });
+}
+
+function renderChart(chartType, chartContext, data) {
+    if (window[chartType]) {
+        window[chartType].destroy();
     }
-    chart = new Chart(stockChart, {
+    window[chartType] = new Chart(chartContext, {
         type: 'line',
         data: {
-            labels: labels,
+            labels: data.labels,
             datasets: [{
                 label: 'Preço de Fechamento',
-                data: data,
+                data: data.dataPoints,
                 borderColor: 'blue',
                 fill: false
             }]
@@ -162,8 +181,11 @@ function handleTabSwitch(event) {
     tab.classList.add('active');
     const contentId = tab.dataset.tab;
     document.getElementById(contentId).classList.add('active');
-}
 
-function showSuggestions() {
-    const query = symbolInput.value.trim().toUpperCase();
+    if (contentId === 'stocks') {
+        fetchStockData('AAPL');
+    } else if (contentId === 'crypto') {
+        fetchCryptoData('bitcoin');
+    }
+    // COLOCAR O RESTO QUE FALTA AQUI!!!
 }
